2d781711161871dcd2fb1b70c7168a3d8fd7b805
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-01-28 02:43:10 +0600

    successfully added data to the db and get data from db

--- app/src/main/java/com/example/todokotlin/MainActivity.kt
+++ app/src/main/java/com/example/todokotlin/MainActivity.kt
@@ -7,40 +7,61 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.todokotlin.data.DatabaseHandler
+import com.example.todokotlin.model.Contact
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var editTextMessage: EditText
-    private lateinit var saveButton: Button
-    private lateinit var textViewMessage: TextView
-//    declaring sharedPreference================
-    private  val PREF_NAME = "myPref"
-    private  lateinit var sharePref: SharedPreferences
+//    private lateinit var editTextMessage: EditText
+//    private lateinit var saveButton: Button
+//    private lateinit var textViewMessage: TextView
+////    declaring sharedPreference================
+//    private  val PREF_NAME = "myPref"
+//    private  lateinit var sharePref: SharedPreferences
+
+    private lateinit var textViewData: TextView
     @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editTextMessage = findViewById(R.id.edit_text_message)
-        saveButton = findViewById(R.id.save_button)
-        textViewMessage = findViewById(R.id.text_view_message)
+//        editTextMessage = findViewById(R.id.edit_text_message)
+//        saveButton = findViewById(R.id.save_button)
+//        textViewMessage = findViewById(R.id.text_view_message)
+//
+//        saveButton.setOnClickListener {
+////            mode = 0 means that is access able
+//            sharePref = getSharedPreferences(PREF_NAME, 0)
+//            val editor = sharePref.edit()
+////key pare the key is hare is message
+//            editor.putString("message",editTextMessage.text.toString())
+//            editor.apply()
+//
+//        }
+//
+//        val prefs = getSharedPreferences(PREF_NAME, 0)
+////        if the message isn't empty then we can get the value
+////        or we will see not found
+//        if(prefs.contains("message")){
+//            val message = prefs.getString("message", "not found")
+//            textViewMessage.text = message
+//        }
 
-        saveButton.setOnClickListener {
-//            mode = 0 means that is access able
-            sharePref = getSharedPreferences(PREF_NAME, 0)
-            val editor = sharePref.edit()
-//key pare the key is hare is message
-            editor.putString("message",editTextMessage.text.toString())
-            editor.apply()
+//    shared preference section done ==================================
+        textViewData = findViewById(R.id.text_view_data)
 
+    val db = DatabaseHandler(this)
+
+    db.addContact(Contact("Rahat", "3294829038"))
+    db.addContact(Contact("Akash", "3478207342"))
+    db.addContact(Contact("Asad", "3242848"))
+    db.addContact(Contact("Niloy", "23845902"))
+
+        val contactList = db.getAllContacts()
+
+        for (contact in contactList){
+            textViewData.append("\nName: ${contact.name}"+"'\nPhoneNumber: ${contact.phoneNumber}")
         }
 
-        val prefs = getSharedPreferences(PREF_NAME, 0)
-//        if the message isn't empty then we can get the value
-//        or we will see not found
-        if(prefs.contains("message")){
-            val message = prefs.getString("message", "not found")
-            textViewMessage.text = message
-        }
 
     }
 }
--- app/src/main/java/com/example/todokotlin/data/DatabaseHandler.kt
+++ app/src/main/java/com/example/todokotlin/data/DatabaseHandler.kt
@@ -1,23 +1,86 @@
 package com.example.todokotlin.data
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.provider.ContactsContract.Contacts
+import com.example.todokotlin.model.Contact
 import com.example.todokotlin.utils.Constants
 
 class  DatabaseHandler(val context: Context): SQLiteOpenHelper(context, Constants.DATABASE_NAME, null, Constants.DATABASE_VERSION){
     override fun onCreate(db: SQLiteDatabase?) {
         val CREATE_TABLE_CONTACTS = "CREATE TABLE ${Constants.TABLE_NAME} (" +
                 "${Constants.KEY_ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "${Constants.KEY_PHONE_NUMBER} TEXT," +
-                "${Constants.KEY_NAME} TEXT)"
+                "${Constants.KEY_NAME} TEXT," +
+                "${Constants.KEY_PHONE_NUMBER} TEXT)"
         db?.execSQL(CREATE_TABLE_CONTACTS)
 
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db?.execSQL("DROP TABLE IF EXISTS ${Constants.TABLE_NAME}" )
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS ${Constants.TABLE_NAME}" )
         onCreate(db)
     }
+
+/*
+* CRUD Operations ================
+* */
+
+    fun addContact(contact: Contact){
+        val db = this.writableDatabase
+
+        val values = ContentValues()
+
+        values.put(Constants.KEY_NAME, contact.name)
+        values.put(Constants.KEY_PHONE_NUMBER, contact.phoneNumber)
+//        insert to row==========
+        db.insert(Constants.TABLE_NAME, null,values)
+//        we need to close the database =========
+        db.close()
+    }
+
+
+//    get individual contact
+    @SuppressLint("Recycle")
+    fun getContact(id: Int): Contact{
+        val db = this.readableDatabase
+        val cursor = db.query(Constants.TABLE_NAME,
+            arrayOf(Constants.KEY_ID, Constants.TABLE_NAME, Constants.KEY_PHONE_NUMBER),
+            Constants.KEY_ID + "=?", arrayOf(id.toString()), null, null, null, null
+            )
+        cursor?.moveToFirst()
+
+        return  Contact(cursor.getString(0).toInt(),cursor.getString(1), cursor.getString(2))
+    }
+
+//    get all contacts =======
+
+    @SuppressLint("Recycle")
+    fun getAllContacts(): MutableList<Contact>{
+        val db = this.readableDatabase
+
+        val contactList = mutableListOf<Contact>()
+
+        val selectAll = "SELECT * FROM ${Constants.TABLE_NAME}"
+        val  cursor = db.rawQuery(selectAll, null)
+        if(cursor.moveToFirst()){
+            do {
+                val contact = Contact(0, "" , "")
+                contact.id = cursor.getString(0).toString().toInt()
+                contact.name = cursor.getString(1)
+                contact.phoneNumber = cursor.getString(2)
+
+// add contact to the contact list
+                contactList.add(contact)
+            } while(cursor.moveToNext())
+        }
+        return  contactList
+    }
+
+
+
+
 
 }
